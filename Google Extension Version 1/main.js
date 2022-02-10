4e58665fa82ee830cb198e4051fa7720ff4f5d7f
Japan101 <69756385+Japan101@users.noreply.github.com>
2022-02-10 22:21:57 +0700

    Add files via upload
    Currently only work with google.com and not google chrome itself

---
+++ Google Extension Version 1/main.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+    var img = document.querySelector("[src='/intl/en_ALL/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png']")
+    var img = document.querySelector("[srcset='/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png 1x, /images/branding/googlelogo/2x/googlelogo_color_272x92dp.png 2x']")
+    img.src = "https://github.com/JPMKL/Legend-of-Zelda-pack/blob/main/LargeLogo.png?raw=true"
+    img.srcset = "https://github.com/JPMKL/Legend-of-Zelda-pack/blob/main/LargeLogo.png?raw=true 1x, https://github.com/JPMKL/Legend-of-Zelda-pack/blob/main/LargeLogo.png?raw=true 2x"
+}
